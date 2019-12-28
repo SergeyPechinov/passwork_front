081656177fe2fbe298eba43ba6b34d5897e4cac3
Sergey Pechinov <sen1c163rus@gmail.com>
2019-12-28 19:06:32 +0400

    fix name colors and container

--- src/App/Containers/ChangeLanguage/Index.jsx
+++ src/App/Containers/ChangeLanguage/Index.jsx
@@ -1,12 +1,16 @@
 import React, {Component} from "react";
 import './Functions';
 import './Style.scss';
+import Button from "../../Components/Button";
 
 class ChangeLanguage extends Component {
 
 	render() {
 		return(
-				<div className={'text-red-dark'}>Выбор языка</div>
+				<div>
+					<Button color={'blue'} value={'РУССКИЙ'}/>
+					<Button color={'red'} value={'РУССКИЙ'}/>
+				</div>
 		);
 	}
 }
